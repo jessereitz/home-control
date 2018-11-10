6ab020a0dfe8ada79f442588fa39b4f798ce3c9e
Jesse Reitz <jessereitz1@gmail.com>
2018-11-10 13:29:28 -0600

    Basic api routing setup.

--- hc-back/index.js
+++ hc-back/index.js
@@ -5,4 +5,9 @@ const port = 8000;
 
 app.get('/', (req, res) => res.send('hello!'));
 
+app.get('/api', (req, res) => {
+  console.log('incoming request');
+  res.send({ user: { username: 'jessereitz', id: '1' } });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- hc-front/src/App.js
+++ hc-front/src/App.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+      fetch('/api')
+        .then(res => res.json())
+        .then((json) => {
+          this.setState({
+            user: json.user
+          });
+        });
+  }
+
   render() {
+    // const user = this.state.user.username ? `, ${this.state.user.username}` : '';
+    console.log(this.state.user);
     return (
       <div className="App">
         <h1>Home Control</h1>
+        {this.state.user ? <h2>Welcome, {this.state.user['username']}</h2> : <h2>Welcome</h2>}
       </div>
     );
   }
